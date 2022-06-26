0c7c4609db89609218a398ff65507073372aa319
Dima Portenko <troublesohard@gmail.com>
2022-06-26 22:44:17 +0300

    first commit

---
+++ patch-json.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const args = process.argv.slice(2);
+const getFilepath = (filename) => `${args[0]}/${filename}`
+// console.log(args)
+
+let fileName = getFilepath('tsconfig.json');
+let file = require(fileName);
+
+file.compilerOptions.module = "es6";
+
+fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
+  if (err) return console.log(err);
+  console.log(JSON.stringify(file));
+  console.log('writing to ' + fileName);
+});
+
+fileName = getFilepath('package.json');
+file = require(fileName);
+
+if (file.resolutions) {
+  file.resolutions["@types/react"] = "^17"
+} else {
+  file.resolutions = {
+    "@types/react": "^17"
+  }
+}
+
+fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
+  if (err) return console.log(err);
+  console.log(JSON.stringify(file));
+  console.log('writing to ' + fileName);
+});
